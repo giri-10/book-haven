0465b9e05afb129fb6346e3782ed05cce3539223
giri-10 <90170719+giri-10@users.noreply.github.com>
2024-09-18 15:10:17 +0530

    initial commit

---
+++ src/test/java/com/giri/bookhaven/BookhavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giri.bookhaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookhavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
