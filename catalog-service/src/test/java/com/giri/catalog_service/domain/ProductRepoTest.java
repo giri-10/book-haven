8069935ba2d1b951b8ff077826fe9bfbe7be154a
giri-10 <90170719+giri-10@users.noreply.github.com>
2024-09-21 23:08:30 +0530

    catalog-service: implemented unit testing for ProductRepository

---
+++ catalog-service/src/test/java/com/giri/catalog_service/domain/ProductRepoTest.java
@@ -0,0 +1,26 @@
+package com.giri.catalog_service.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@DataJpaTest(
+        properties = {
+            "spring.test.database.replace=none",
+            "spring.datasource.url=jdbc:tc:postgresql:16-alpine:///db",
+        })
+@Sql("/test-data.sql")
+class ProductRepoTest {
+    @Autowired
+    private ProductRepo productRepo;
+
+    @Test
+    void shouldGetAllProducts() {
+        List<ProductEntity> products = productRepo.findAll();
+        assertThat(products).hasSize(15);
+    }
+}
